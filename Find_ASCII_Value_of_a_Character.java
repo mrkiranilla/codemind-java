493a6241138dc7bc4a3792efadcbe11c5b2bfc44
mrkiranilla <120078089+mrkiranilla@users.noreply.github.com>
2023-08-25 09:41:26 +0530

    import java.util.*; public class area{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String a=sc.next();         System.out.print((int)a.charAt(0));     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class area{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.print((int)a.charAt(0));
+    }
+}
