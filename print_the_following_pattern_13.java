70260b63811459ae4e58b9718e5f96d0aea3dd0e
mrkiranilla <120078089+mrkiranilla@users.noreply.github.com>
2024-02-13 14:41:05 +0530

    import java.util.*; public class pat{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=i;j++)             {                 System.out.print(j);             }             System.out.println();         }     } }

--- print_the_following_pattern_13.java
+++ print_the_following_pattern_13.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-public class patt{
-    public static void main(String args[]){
+import java.util.*;
+public class pat{
+    public static void main(String args[])
+    {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=n;i++)
+        {
             for(int j=1;j<=i;j++)
             {
                 System.out.print(j);
             }
-            System.out.printf("
-");
+            System.out.println();
         }
-
     }
 }
